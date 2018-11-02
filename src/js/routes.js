b93e7111cac903871e5fe8d23824e5aaa256b2d1
Mohit Kumar Jha <mohitkumarjha56@gmail.com>
2018-11-02 20:00:16 +0530

    added patient,doctor & retailer page

--- src/js/routes.js
+++ src/js/routes.js
@@ -18,6 +18,18 @@ angular.module('RDash').config(['$stateProvider', '$urlRouterProvider',
             .state('tables', {
                 url: '/tables',
                 templateUrl: 'templates/tables.html'
+            })
+            .state('patient', {
+                url: '/patient',
+                templateUrl: 'templates/profile_patient.html'
+            })
+            .state('doctor', {
+                url: '/doctor',
+                templateUrl: 'templates/profile_doctor.html'
+            })
+            .state('retailer', {
+                url: '/retailer',
+                templateUrl: 'templates/profile_retailer.html'
             });
     }
 ]);
