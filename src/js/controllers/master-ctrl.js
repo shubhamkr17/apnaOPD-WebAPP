2b3d7210a1b1db498b25622ad0ed1fafa14ddeae
shubhamkr17 <sk767285@gmail.com>
2018-11-03 19:22:52 +0530

    added search options for doctor,retailer etc on home page

--- src/js/controllers/master-ctrl.js
+++ src/js/controllers/master-ctrl.js
@@ -3,9 +3,9 @@
  */
 
 angular.module('RDash')
-    .controller('MasterCtrl', ['$scope', '$cookieStore', MasterCtrl]);
+    .controller('MasterCtrl', ['$scope', '$cookieStore', '$http',MasterCtrl]);
 
-function MasterCtrl($scope, $cookieStore) {
+function MasterCtrl($scope, $cookieStore,$http) {
     /**
      * Sidebar Toggle & Cookie Control
      */
@@ -32,6 +32,56 @@ function MasterCtrl($scope, $cookieStore) {
         $scope.toggle = !$scope.toggle;
         $cookieStore.put('toggle', $scope.toggle);
     };
+
+
+    ///for toggle between search doctor/medicine/retailer/Locations
+    $scope.showDoctor=true;
+    $scope.searchDoctor = function(){
+      $scope.showDoctor=true;
+      $scope.showRetailer=false;
+      $scope.showMedicine=false;
+      $scope.showLocation=false;
+    };
+    $scope.searchRetailer = function(){
+      $scope.showDoctor=false;
+      $scope.showRetailer=true;
+      $scope.showMedicine=false;
+      $scope.showLocation=false;
+    };
+    $scope.searchMedicine = function(){
+      $scope.showDoctor=false;
+      $scope.showRetailer=false;
+      $scope.showMedicine=true;
+      $scope.showLocation=false;
+    };
+    $scope.searchLocation = function(){
+      $scope.showDoctor=false;
+      $scope.showRetailer=false;
+      $scope.showMedicine=false;
+      $scope.showLocation=true;
+    };
+
+    $http.get("http://172.16.1.47:3000/api/doctors")
+    .then(function(response){
+      console.log(response);
+      $scope.doctors=response.data;
+    });
+    $http.get("http://172.16.1.47:3000/api/retailers")
+    .then(function(response){
+      console.log(response);
+      $scope.retailers=response.data;
+    });
+    $http.get("http://172.16.1.47:3000/api/medicines")
+    .then(function(response){
+      console.log(response);
+      $scope.medicines=response.data;
+    });
+    // $http.get("http://172.16.1.47:3000/api/locations")
+    // .then(function(response){
+    //   console.log(response);
+    //   $scope.locations=response.data;
+    // });
+
 
     window.onresize = function() {
         $scope.$apply();
